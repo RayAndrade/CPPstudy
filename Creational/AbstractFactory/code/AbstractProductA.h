5e7e5e104d96da02b644e7c59f0a0789f49521d6
Ray Andrade <TheRayCode@RayAndrade.com>
2025-03-19 13:10:26 -0700

    *

---
+++ Creational/AbstractFactory/code/AbstractProductA.h
@@ -0,0 +1,13 @@
+//
+// Created by ray on 3/16/25.
+//
+
+#ifndef CODE_ABSTRACTPRODUCTA_H
+#define CODE_ABSTRACTPRODUCTA_H
+
+class AbstractProductA {
+public:
+    virtual void use() = 0; // Pure virtual function
+    virtual ~AbstractProductA() {} // Virtual destructor
+};
+#endif //CODE_ABSTRACTPRODUCTA_H
--- Creational/AbstractFactory/code/main.cpp
+++ Creational/AbstractFactory/code/main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
-
+#include "ConcreteFactory1.h"
+#include "ConcreteFactory2.h"
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "TheRayCode";
-    std::cout <<  lang << " is AWESOME!!!\n";
+    AbstractFactory* factory1 = new ConcreteFactory1();
+    AbstractProductA* productA1 = factory1->createProductA();
+    AbstractProductB* productB1 = factory1->createProductB();
+
+    productA1->use();
+    productB1->use();
+
+    AbstractFactory* factory2 = new ConcreteFactory2();
+    AbstractProductA* productA2 = factory2->createProductA();
+    AbstractProductB* productB2 = factory2->createProductB();
+
+    productA2->use();
+    productB2->use();
+
+    delete productA1;
+    delete productB1;
+    delete factory1;
+
+    delete productA2;
+    delete productB2;
+    delete factory2;
 
     return 0;
 }
